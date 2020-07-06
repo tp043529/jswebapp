4d59a73d27bc5ee35a4285fb01fc9a90ba92ca17
tp043529 <67571187+tp043529@users.noreply.github.com>
2020-07-06 12:16:23 +0800

    Update list_of_restaurants.php

--- list_of_restaurants.php
+++ list_of_restaurants.php
@@ -13,35 +13,35 @@
           <th>Address</th>                    
           <th>Phone</th>                   
         </tr>                                 
+          <?php        
+          $serverName = "simplewebtp043529.database.windows.net";         
+          $connectionOptions = array(                             
+            "Database" => "simpleweb",                             
+            "Uid" => "simpleweb",                             
+            "PWD" => "Simple@web");                          
+          //Establishes the connection        
+          $conn = sqlsrv_connect($serverName, $connectionOptions);         
+          if (!$conn) {            
+            die("Error connection: ".sqlsrv_errors());        
+          }                
+          $tsql= "SELECT * FROM [dbo].[restaurant]";        
+          $getResults= sqlsrv_query($conn, $tsql);                           
+          if ($getResults == FALSE)  
+          {             
+            die(sqlsrv_errors());   
+          }                          
+
+                 while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC))   
+                 {             
+                   echo "<tr>";             
+                   echo "<td>". $row['restaurant_id'] . "</td>";             
+                   echo "<td>". $row['restaurant_name'] ."</td>";             
+                   echo "<td>". $row['restaurant_address'] . "</td>";            
+                   echo "<td>". $row['restaurant_phone'] . "</td>";             
+                   echo "</tr>";        
+                 }        
+          sqlsrv_free_stmt($getResults); ?> 
       </table>           
     </center>     
   </body> 
 </html>
-<?php        
-$serverName = "simplewebtp043529.database.windows.net";         
-$connectionOptions = array(                             
-  "Database" => "simpleweb",                             
-  "Uid" => "simpleweb",                             
-  "PWD" => "Simple@web");                          
-//Establishes the connection        
-$conn = sqlsrv_connect($serverName, $connectionOptions);         
-if (!$conn) {            
-  die("Error connection: ".sqlsrv_errors());        
-}                
-$tsql= "SELECT * FROM [dbo].[restaurant]";        
-$getResults= sqlsrv_query($conn, $tsql);                           
-if ($getResults == FALSE)  
-{             
-  die(sqlsrv_errors());   
-}                          
- 
-       while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC))   
-       {             
-         echo "<tr>";             
-         echo "<td>". $row['restaurant_id'] . "</td>";             
-         echo "<td>". $row['restaurant_name'] ."</td>";             
-         echo "<td>". $row['restaurant_address'] . "</td>";            
-         echo "<td>". $row['restaurant_phone'] . "</td>";             
-         echo "</tr>";        
-       }        
-sqlsrv_free_stmt($getResults); ?> 
