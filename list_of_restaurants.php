1af92ec311125357c86cd1c14900c421a035edb0
tp043529 <67571187+tp043529@users.noreply.github.com>
2020-07-06 12:14:11 +0800

    Update list_of_restaurants.php

--- list_of_restaurants.php
+++ list_of_restaurants.php
@@ -1,3 +1,22 @@
+<html>     
+  <head>           
+    <title>List of KL Restaurant</title>     
+  </head>     
+  <body>           
+    <center>               
+      <h1>List of KL Restaurant</h1>               
+      <!--create table structure using HTML first-->               
+      <table border="1">                   
+        <tr>                     
+          <th>Restaurant ID</th>                     
+          <th>Restaurant Name</th>                     
+          <th>Address</th>                    
+          <th>Phone</th>                   
+        </tr>                                 
+      </table>           
+    </center>     
+  </body> 
+</html>
 <?php        
 $serverName = "simplewebtp043529.database.windows.net";         
 $connectionOptions = array(                             
